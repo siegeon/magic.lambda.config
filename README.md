6224cf7baf284d9a970cef549c1742792ba3d7e9
Thomas Hansen <thomas@gaiasoul.com>
2019-09-26 16:30:06 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# magic.lambda.config
-Configuration plugin for magic.lambda
+
+# Magic Lambda Config for .Net
+
+[![Build status](https://travis-ci.org/polterguy/magic.lambda.config.svg?master)](https://travis-ci.org/polterguy/magic.lambda.config)
+
+To be provided ...
---
+++ magic.lambda.config/Config.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using magic.node;
+using magic.node.extensions;
+using magic.signals.contracts;
+
+namespace magic.lambda
+{
+    [Slot(Name = "config")]
+    public class Config : ISlot
+    {
+        readonly IConfiguration _configuration;
+
+        public Config(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public void Signal(ISignaler signaler, Node input)
+        {
+            if (input.Children.Any())
+                throw new ApplicationException("[config] cannot handle children nodes");
+
+            input.Value = _configuration[input.GetEx<string>()];
+        }
+    }
+}
