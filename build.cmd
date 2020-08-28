0cff0d72708b3c82f241f9db4f4df74592bb97e2
Thomas Hansen <thomas@gaiasoul.com>
2020-08-28 11:05:40 +0300

    Updating version, getting ready for release

--- build.cmd
+++
@@ -1,7 +0,0 @@
-
-set version=%1
-set key=%2
-
-cd %~dp0
-dotnet build magic.lambda.config/magic.lambda.config.csproj --configuration Release --source https://api.nuget.org/v3/index.json
-dotnet nuget push magic.lambda.config/bin/Release/magic.lambda.config.%version%.nupkg -k %key% -s https://api.nuget.org/v3/index.json
