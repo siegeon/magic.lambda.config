5b650030efb68a5d33adaad55b9c0599b77fd68a
Thomas Hansen <thomas@gaiasoul.com>
2020-09-06 21:54:32 +0300

    Increasing coverage

--- magic.lambda.config.tests/ConfigTests.cs
+++ magic.lambda.config.tests/ConfigTests.cs
@@ -3,6 +3,7 @@
  * See the enclosed LICENSE file for details.
  */
 
+using System;
 using Xunit;
 using magic.node;
 using magic.node.extensions;
@@ -18,6 +19,14 @@ namespace magic.lambda.config.tests
             var args = new Node("", "foo-value");
             signaler.Signal("config.get", args);
             Assert.Equal("bar-xx", args.Get<string>());
+        }
+
+        [Fact]
+        public void CheckConfigurationSettingThrows()
+        {
+            var signaler = Common.Initialize();
+            var args = new Node("");
+            Assert.Throws<ArgumentException>(() => signaler.Signal("config.get", args));
         }
     }
 }
