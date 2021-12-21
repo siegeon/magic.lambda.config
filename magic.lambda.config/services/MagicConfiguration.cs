8c5d97cca100329eed4f074715fc255380717034
Thomas Hansen <thomas@gaiasoul.com>
2021-12-21 14:50:51 +0200

    Redirecting Magic configuration to IConfiguration for simplicity

--- magic.lambda.config/services/MagicConfiguration.cs
+++ magic.lambda.config/services/MagicConfiguration.cs
@@ -2,8 +2,7 @@
  * Magic Cloud, copyright Aista, Ltd. See the attached LICENSE file for details.
  */
 
-using System.Linq;
-using Newtonsoft.Json.Linq;
+using Microsoft.Extensions.Configuration;
 using magic.node.contracts;
 
 namespace magic.lambda.config.services
@@ -14,53 +13,21 @@ namespace magic.lambda.config.services
     /// </summary>
     public class MagicConfiguration : IMagicConfiguration
     {
-        readonly IFileService _fileService;
-        readonly IRootResolver _rootService;
-        readonly JObject _config;
+        readonly IConfiguration _configuration;
 
         /// <summary>
         /// Creates an instance of your type.
         /// </summary>
-        /// <param name="fileService">Needed to resolve configuration file.</param>
-        /// <param name="rootService">Needed to namespace configuration file as we resolve it.</param>
-        public MagicConfiguration(IFileService fileService, IRootResolver rootService)
+        /// <param name="configuration">Actual configuration object.</param>
+        public MagicConfiguration(IConfiguration configuration)
         {
-            _fileService = fileService;
-            _rootService = rootService;
-            var json = _fileService.Load(_rootService.RootFolder + "config/appsettings.json");
-            _config = JObject.Parse(json);
+            _configuration = configuration;
         }
 
         /// <inheritdoc />
         public string this[string key]
         {
-            get => Find(key)?.Value<string>();
-            set
-            {
-                var jObject = Find(key);
-                jObject.Value = value;
-                _fileService.Save(_rootService.RootFolder + "config/appsettings.json", _config.ToString());
-            }
+            get => _configuration[key];
         }
-
-        #region [ -- Private helpers -- ]
-
-        /*
-         * Returns a JProperty given the specified "path".
-         */
-        JValue Find(string key)
-        {
-            var splits = key.Split(':');
-            var current = _config;
-            foreach (var idxEntity in splits.Take(splits.Length - 1))
-            {
-                current = current[idxEntity] as JObject;
-                if (current == null)
-                    return null;
-            }
-            return current[splits.Last()] as JValue;
-        }
-
-        #endregion
     }
 }
